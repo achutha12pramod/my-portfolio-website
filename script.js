1b2658d8bbc0a9496c47517a2d717fd823727d5c
achutha12pramod <achuthanachu694@gmail.com>
2025-06-15 10:25:21 +0530

    Made few updates

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+const element = document.querySelector('.section_text_p2');
+const length = element.textContent.length;
+
+// Use ch units — 1ch = width of '0' in monospace
+element.style.setProperty('--typing-steps', length);
+element.style.setProperty('--typing-duration', `${length * 0.1}s`);
+element.style.width = `${length}ch`; // Ensures cursor ends at last letter
+});
+
+const btn = document.querySelector("#btnemail");
+        const btnText = document.querySelector("#btnText");
+
+        btn.onclick = () => {
+            btnText.innerHTML = "Thanks";
+            btn.classList.add("active");
+        };
